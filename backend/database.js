f712c7383e9c67a10412c7ef475aa6ce17a1bf52
unknown <rizner.josip@gmail.com>
2022-04-13 11:13:52 +0200

    Connected with database

--- backend/database.js
+++ backend/database.js
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
+dotenv.config()
+
 export default function connectToDatabase(){
     mongoose.connect(process.env.MONGO_DB_URI)
-    mongoose.connection.on("connected", console.log("Database is successfully connected!"))
-    mongoose.connection.on("disconnected", console.log("Database is disconnected!"))
-    mongoose.connection.on("error", console.error("There was error with connecting to database!"))
+    mongoose.connection.on("connected",() => {console.log("Database is successfully connected!")})
+    mongoose.connection.on("disconnected", ()=>{console.log("Database is disconnected!")})
+    mongoose.connection.on("error", ()=>{console.error("There was error with connecting to database!")})
 }
