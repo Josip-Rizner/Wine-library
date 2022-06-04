f585ff6a8eec84528461baa2f8ae0b162905d632
istakovic <ivan.stakovic@hotmail.com>
2022-06-04 11:08:53 +0200

    add and edit view for wines implementation

--- frontend/mainPage/script.js
+++ frontend/mainPage/script.js
@@ -1,3 +1,6 @@
-$(function(){
-    
+var myModal = document.getElementById('exampleModal')
+var myInput = document.getElementById('add')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
 })
