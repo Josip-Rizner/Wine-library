de2322fe847c1281ca10ed8117ea1356f7d1baf3
istakovic <ivan.stakovic@hotmail.com>
2022-06-11 15:58:46 +0200

    details improvment

--- frontend/mainPage/script.js
+++ frontend/mainPage/script.js
@@ -17,14 +17,13 @@ $(function () {
         $('#detailModal').modal('show')
       }
     }
-    console.log($(this).get(0).id)
   })
   $.get("http://localhost:3000/wines", function (data) {
     dataForUse = data;
 
     for (let i = 0; i < data['message'].length; i++) {
 
-      $(".wines").append('<div class="col-md-6 col-lg-4 col-xl-3"> <div id="product-1" class="single-product" style="background: url(' + [data['message'][i].imageUrl] + ') no-repeat center; width: auto;"> <div class="part-1"> <span class="discount">' + data['message'][i].year + "    " + "Type:  " + data['message'][i].type + '</span> <ul> <li><a href="#"><i class="fas fa-trash" id="delete" data-bs-toggle="modal" data-bs-target="#deleteModal"></i></a></li> <li><a href="#" class="detalji" id="' + data['message'][i]._id + '"><i class="fa fa-bars" id="details"></i></a></li> </ul> </div> <div class="part-2"> <h3 class="product-title">' + data['message'][i].name + '</h3> <h4 class="product-price">' + data['message'][i].price + "$" + '</h4> <h5 class="product-price">' + "ABV: " + data['message'][i].ABV + "%" + '</h5> <h6 class="product-price">' + "Color: " + data['message'][i].color + '</h6> <h6 class="product-price">' + "Country: " + data['message'][i].country + '</h6> <h6 class="product-price">' + "Size: " + data['message'][i].size + "     mil" + '</h6></div> </div> </div>')
+      $(".wines").append('<div class="col-md-6 col-lg-4 col-xl-3"> <div id="product-1" class="single-product" style="background: url(' + [data['message'][i].imageUrl] + ') no-repeat center; width: auto;"> <div class="part-1"> <span class="discount">'+ "Name:  " + data['message'][i].name + '</span> <ul> <li><a href="#"><i class="fas fa-trash" id="delete" data-bs-toggle="modal" data-bs-target="#deleteModal"></i></a></li> <li><a href="#" class="detalji" id="' + data['message'][i]._id + '"><i class="fa fa-bars" id="details"></i></a></li> </ul> </div> <div class="part-2"> <h3 class="product-title mt-5">' + data['message'][i].price +"$" +  '</h3> <h4 class="product-price">' + data['message'][i].type +'</h4> <h5 class="product-price"></div> </div> </div>')
     }
   });
 })
